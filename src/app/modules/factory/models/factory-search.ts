c34aec48d5bc81e418161025306aaeed5a226385
Gghazy <a.ghazy@automationxgroup.com>
2024-03-11 06:05:30 +0200

    ...

--- src/app/modules/factory/models/factory-search.ts
+++ src/app/modules/factory/models/factory-search.ts
@@ -1,5 +1,5 @@
 import { SearchCriteria } from "src/app/core/models/search-criteria";
 
 export class FactorySearch extends SearchCriteria {
-    OwnerIdentity:string="1012966132"
+    OwnerIdentity:string="1012955132"
 }
