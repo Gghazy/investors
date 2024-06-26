0a36cecccedc2161ee159583fb188e8aba37ac9a
Doha-Khair <Doha@DESKTOP-3R1LKG9>
2024-06-26 14:04:11 +0300

    update status

--- src/app/modules/factory/components/factory-list/factory-list.component.ts
+++ src/app/modules/factory/components/factory-list/factory-list.component.ts
@@ -5,6 +5,8 @@ import { FactorySearch } from '../../models/factory-search';
 import { FactoryModel } from '../../models/factory-model';
 import { ResultResponse } from 'src/app/core/models/result-response';
 import { SharedService } from 'src/app/shared/services/shared.service';
+import { FactoryLandingService } from 'src/app/modules/factory-landing/factory-landing.service';
+import { Observable, forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-factory-list',
@@ -16,32 +18,47 @@ import { SharedService } from 'src/app/shared/services/shared.service';
 })
 export class FactoryListComponent implements OnInit {
 
-  search=new FactorySearch(); 
-
+  search = new FactorySearch();
+  factoryStatus: boolean = false
+  factoryUpdateStatus: any[]=[]
+  dataFactory:any[]=[]
   factories = new ResultResponse<FactoryModel>();
 
-  constructor(private factoryService :FactoryService,
+  constructor(private factoryService: FactoryService,
+    private factoryLandingService: FactoryLandingService,
     private shared: SharedService,
-  ){
+  ) {
 
   }
 
   ngOnInit(): void {
-   this.getFactories();
-   this.shared.setUserRole('Investor');
-  } 
+    this.getFactories();
+    this.shared.setUserRole('Investor');
+  }
 
-  getFactories() { 
-    
+  getFactories() {
+
     this.factoryService
       .getAllPagination(this.search)
       .subscribe((res: any) => {
         this.factories = res.Data;
+        this.factories.Items.forEach(element => {
+          this.getFactoryUpdateStatus(element.Id)
+     
+          });
       });
   }
-
-  pageChanged(data:any){
-    this.search.PageNumber=data;
+  getFactoryUpdateStatus(id:number){
+    this.factoryLandingService
+      .CheckFactoryUpdateStatus(id)
+      .subscribe((res: any) => {
+        this.factoryUpdateStatus= res.Data;
+        this.dataFactory.push(this.factoryUpdateStatus)
+       console.log(this.dataFactory)
+      });
+  } 
+  pageChanged(data: any) {
+    this.search.PageNumber = data;
     this.getFactories();
 
   }
