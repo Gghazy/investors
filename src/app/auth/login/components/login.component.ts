2cfa02459ecf3ecdb066a522820cf553c7b27123
lobaba <lobaba.eltayeb@gmail.com>
2024-07-21 21:10:13 +0300

    test redirect

--- src/app/auth/login/components/login.component.ts
+++ src/app/auth/login/components/login.component.ts
@@ -46,9 +46,10 @@ else
       this.request = res.Data;
 
       let externallogin='https://mim.gov.sa/sso/'+this.request.uuid+'/login?token='+this.request.token+'&signature='+this.request.signature;
-      window.location.href=externallogin;
-      
-
+     // window.location.href=externallogin;
+      //window.location.href="https://localhost:44343/api/CallApi"
+      //window.location.href="https://localhost:44343/api/CallApi"
+      window.location.href="http://sprime-001-site25.atempurl.com/api/sso?url=http://sprime-001-site25.atempurl.com/pages/factories-list"
     });
 }
       
