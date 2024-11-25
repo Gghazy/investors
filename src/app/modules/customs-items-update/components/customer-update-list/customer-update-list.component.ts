f1c517adfb06df88932b31182b7820e647ee7544
Lobabaty <lobaba.eltayeb@gmail.com>
2024-11-25 15:28:22 +0300

    year in all tab

--- src/app/modules/customs-items-update/components/customer-update-list/customer-update-list.component.ts
+++ src/app/modules/customs-items-update/components/customer-update-list/customer-update-list.component.ts
@@ -78,7 +78,7 @@ year!:number;
     this.periodService
     .getOne(this.periodId)
     .subscribe((res: any) => {
-      this.year = res.Data.Year -1;
+      this.year = res.Data.Year;
       this.PeriodName= res.Data.PeriodName;
     });
   }
